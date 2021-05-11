70c55a2863a05668db34ce4a1c482e6e2ae2ff44
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-05-11 19:44:03 -0300

    Criado os fragments a ser utilizado, configurado a bottom navigation. Criado os ícones de início, pesquisa, biblioteca e configuração

---
+++ SpotifyClone/app/src/main/java/com/davisilvaprojetos/spotifyclone/activity/MainActivity.java
@@ -0,0 +1,74 @@
+package com.davisilvaprojetos.spotifyclone.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.davisilvaprojetos.spotifyclone.R;
+import com.davisilvaprojetos.spotifyclone.fragment.HomeFragment;
+import com.davisilvaprojetos.spotifyclone.fragment.LibraryFragment;
+import com.davisilvaprojetos.spotifyclone.fragment.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        configuraBottomNavigationView();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.viewPager,new HomeFragment()).commit();
+
+    }
+
+    private void configuraBottomNavigationView(){
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
+        //Configurações iniciais
+        /*bottomNavigationViewEx.enableAnimation(true);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);*/
+
+        habilitarNavegacao(bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);
+
+    }
+
+    private void habilitarNavegacao(BottomNavigationViewEx viewEx){
+        viewEx.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                switch (item.getItemId()){
+                    case R.id.ic_home:
+                        fragmentTransaction.replace(R.id.viewPager,new HomeFragment()).commit();
+                        return true;
+
+                    case R.id.ic_search:
+                        fragmentTransaction.replace(R.id.viewPager,new SearchFragment()).commit();
+                        return true;
+
+                    case R.id.ic_library:
+                        fragmentTransaction.replace(R.id.viewPager,new LibraryFragment()).commit();
+                        return true;
+
+
+                }
+                return false;
+            }
+        });
+    }
+}
